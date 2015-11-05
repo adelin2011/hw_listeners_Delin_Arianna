28458329087461436a7dfa736e8ec07993833967
Arianna Delin <arianna.delin11@ufl.edu>
2015-11-04 21:41:11 -0500

    Added new eventListener to JS file

--- js/main.js
+++ js/main.js
@@ -1,1 +1,10 @@
-h1.addEventListener(click, function); 
+var pageheader = document.getElementById("heading"); 
+	heading.addEventListener("click," function modifyHTML(){
+		console.log("You clicked me");
+	}
+
+var paragraph = document.createElement("p"); 
+var node = document.createTextNode ("This is click number x"); 
+paragraph.appendChild(node); 
+var element = document.getElementById("div1"); 
+element.appendChild(paragraph); 	
