91b584636e7b834485f6f7bd17081ab4a63be22a
Arianna Delin <arianna.delin11@ufl.edu>
2015-11-05 22:54:41 -0500

    updated Event Listeners in JS as well as HTML.

--- js/main.js
+++ js/main.js
@@ -8,7 +8,14 @@ image.addEventListener("click", function(){
 	console.log("This class is great"); 
 }); 
 
-var paragraph = document.getElementById("graph"); 
-graph.addEventListener("click", function(){
-	console.log("smiley face"); 
-}); 
+var graph = document.getElementById("graph"); 
+graph.addEventListener("focus", function(event){
+	event.target.style.background = "purple"; 
+}, true); 
+graph.addEventListener("blur", function (event){
+	event.target.style.background = ""; 
+}, true); 
+
+//First EventListener is for the header. This mouseenter changes it to red when it is moused over.
+//Second EventListener is for the image. This watches for when the image is clicked on, it produces the string "This class is great." 
+//Third EventListener is for the paragraph. This plaes the text in and out of focus. 
