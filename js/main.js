4d0147a55e194b0984b5649bf9e619234e8b26b3
Arianna Delin <arianna.delin11@ufl.edu>
2015-11-06 20:40:56 -0500

    Updated Event Listeners in the custom branch. Explained what each event is for.

--- js/main.js
+++ js/main.js
@@ -3,19 +3,19 @@ heading.addEventListener("mouseenter", function ( event) {
 	event.target.style.color = "red"; 
 }); 
 
+var button = document.getElementById("mybtn"); 
+mybtn.addEventListener("click", displayDate); 
+function displayDate(){
+	document.getElementById("graph").innerHTML = Date(); 
+}
+
 var image = document.getElementById("image"); 
-image.addEventListener("click", function(){
-	console.log("This class is great"); 
+image.addEventListener("click", function modifyHTML(){
+	var image = document.getElementById("image"); 
+	image.innerHTML = "You clicked on me!"; 
 }); 
 
-var graph = document.getElementById("graph"); 
-graph.addEventListener("focus", function(event){
-	event.target.style.background = "purple"; 
-}, true); 
-graph.addEventListener("blur", function (event){
-	event.target.style.background = ""; 
-}, true); 
 
 //First EventListener is for the header. This mouseenter changes it to red when it is moused over.
-//Second EventListener is for the image. This watches for when the image is clicked on, it produces the string "This class is great." 
-//Third EventListener is for the paragraph. This plaes the text in and out of focus. 
+//Second EventListener is for the button. This changed the paragraph under it to the current date. 
+//Third EventListener is for the image. This changes the image to a message when it is clicked on. 
